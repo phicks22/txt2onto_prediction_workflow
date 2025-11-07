b0ebaf874e73d8194f090574d866562671740b2b
Junxia Lin <jlin18@xsede.org@login-ci1.rc.int.colorado.edu>
2025-11-07 14:22:38 -0700

    Updated the annotation analysis script.

--- src/threshold_annotation_analysis.py
+++ src/threshold_annotation_analysis.py
@@ -9,7 +9,6 @@ Date: 2025-06-27
 import polars as pl
 from glob import glob
 import numpy as np
-import pandas as pd
 import os
 import json
 from pathlib import Path
@@ -20,7 +19,10 @@ from argparse import ArgumentParser
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
-        "-label_dir", help="Path to the label data", required=True, type=str
+        "-label_dir", 
+        help="Path to the label data", 
+        required=True, 
+        type=str
     )
     parser.add_argument(
         "-prob_dir",
@@ -41,24 +43,33 @@ if __name__ == "__main__":
         type=str,
     )
     parser.add_argument(
+        "-index_col",
+        help="Column name storing index IDs",
+        default="index",
+        type=str,
+    )
+    parser.add_argument(
         "-is_study",
         help="Flag if the data is sample-level or study-level",
         default=False,
         type=bool,
     )
     parser.add_argument(
-        "-outdir", help="outdir to calculated data", required=True, type=str
+        "-outdir", 
+        help="outdir to calculated data", 
+        required=True, 
+        type=str
     )
     args = parser.parse_args()
 
     label_data = pl.scan_parquet(args.label_dir)
     annotations = pl.scan_parquet(args.annotations)
-    best_th_df = pd.read_csv(args.best_threshold, sep="\t")
+    best_th_df = pl.read_csv(args.best_threshold, separator="\t")
     best_th_df = best_th_df[best_th_df["task"] != "task"].reset_index(drop=True)
 
-    prior = [pd.NA] * len(best_th_df["task"])
-    num_pos = [pd.NA] * len(best_th_df["task"])
-    num_pred_pos = [pd.NA] * len(best_th_df["task"])
+    prior = [None] * len(best_th_df["task"])
+    num_pos = [None] * len(best_th_df["task"])
+    num_pred_pos = [None] * len(best_th_df["task"])
 
     # loop over the prediction file of each term
     for file in tqdm(
@@ -72,7 +83,7 @@ if __name__ == "__main__":
             # Remove rows where the label is 0
             # Convert negative values to zero in the label column
             columns_to_select = [] if args.is_study else ["group"]
-            selected_columns = columns_to_select + ["index", task]
+            selected_columns = columns_to_select + [args.index_col, task]
             label_data_sub = (
                 label_data.select(selected_columns)
                 .filter(pl.col(task) != 0)
@@ -80,14 +91,11 @@ if __name__ == "__main__":
                 .collect()
             )
 
-            print("pred_data and label_data_sub")
-            print(pred_data)
-            print(label_data_sub)
             # merge the prediction data to label data
             pred_label_data = pred_data.select(["ID", "prob"]).join(
-                label_data_sub, left_on="ID", right_on="index", how="right"
+                label_data_sub, left_on="ID", right_on=args.index_col, how="right"
             )
-            selected_columns = columns_to_select + ["index", task, "prob"]
+            selected_columns = columns_to_select + [args.index_col, task, "prob"]
             pred_label_data = pred_label_data.select(selected_columns)
 
             # drop the rows with na (there could be NA in prob).
@@ -97,7 +105,7 @@ if __name__ == "__main__":
             
             # subset the annotaion data
             annotation_sub = annotations.select(["ID", task]).collect()
-            annotation_sub = annotation_sub.rename({"ID": "index"})
+            annotation_sub = annotation_sub.rename({"ID": args.index_col})
             print("annotation_sub")
             print(annotation_sub)
 
@@ -114,8 +122,8 @@ if __name__ == "__main__":
             num_pos[index] = num_pos_value
             num_pred_pos[index] = num_pred_pos_value
 
-    # save the data (change: use polars dataframe)
-    combined_data = pd.DataFrame(
+    # save the data
+    combined_data = pl.DataFrame(
         {
             "task": best_th_df["task"],
             "best_threshold": best_th_df["best_threshold"],
@@ -124,4 +132,4 @@ if __name__ == "__main__":
             "num_of_pred_pos": num_pred_pos,
         }
     )
-    combined_data.to_csv(args.outdir, index=False)
+    combined_data.write_csv(args.outdir)
